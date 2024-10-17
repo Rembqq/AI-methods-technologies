0114df40b8dde8ac00ca128061d33c6934ed2f1d
Rembqq <nikitavolkovskyi@gmail.com>
2024-10-17 17:58:10 +0300

    complement + polinomial + prob-conj-disj + minmax

--- src/complement.py
+++ src/complement.py
@@ -4,28 +4,15 @@ import matplotlib.pyplot as plt
 
 # Вихідна нечітка множина
 x = np.linspace(0, 10, 100)
-y_ = np.exp(-0.1 * (x - 5)**2)  # Функція приналежності нечіткої множини A
+v1 = np.exp(-0.1 * (x - 5)**2)
 
 # Доповнення нечіткої множини
-def complement(mu_A):
-    return 1 - mu_A
-
-# Обчислюємо доповнення
-y_complement = complement(y_)
+complement = 1 - v1
 
 # Візуалізація
-plt.figure(figsize=(8, 5))
-
-# Вихідна нечітка множина
-plt.plot(x, y_, 'k-', label='Нечітка множина Y')
-
-# Доповнення нечіткої множини
-plt.plot(x, y_complement, 'r-', label='Доповнення Y (1 - y*)')
-
+plt.plot(x, v1, label='Нечітка множина A')
+plt.plot(x, complement, label='Доповнення A')
 plt.title('Доповнення нечіткої множини')
-plt.xlabel('x')
-plt.ylabel('Ступінь приналежності')
-plt.ylim([0, 1])
 plt.legend()
-
 plt.show()
+
---
+++ src/probablistic-conj-disj.py
@@ -0,0 +1,21 @@
+import numpy as np
+import skfuzzy as fuzz
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 100)
+
+v1 = np.exp(-0.1 * (x - 5)**2)
+v2 = np.exp(-0.05 * (x - 5)**2)
+
+# Вірогідна кон'юнкція: вірогідний мінімум
+probabilistic_conjunction = v1 * v2
+
+# Вірогідна диз'юнкція: вірогідний максимум
+probabilistic_disjunction = v1 + v2 - v1 * v2
+
+# Візуалізація
+plt.plot(x, probabilistic_conjunction, label='Вірогідна Кон\'юнкція')
+plt.plot(x, probabilistic_disjunction, label='Вірогідна Диз\'юнкція')
+plt.title('Вірогідна інтерпретація')
+plt.legend()
+plt.show()
